17597e3d3cbb92383fda6b779bbbe10713049a46
Schnitz <12687466+CptSchnitz@users.noreply.github.com>
2025-12-31 15:34:26 +0200

    feat(tracing): added

---
+++ packages/tracing/src/common/packageInfoLoader.ts
@@ -0,0 +1,22 @@
+import { readPackageJsonSync } from '@map-colonies/read-pkg';
+
+interface PackageInfo {
+  name: string;
+  version: string;
+}
+
+let packageInfo: PackageInfo | undefined = undefined;
+
+function loadPackageInfo(): PackageInfo {
+  if (packageInfo) {
+    return packageInfo;
+  }
+  const packageJSON = readPackageJsonSync();
+  packageInfo = {
+    name: packageJSON.name ?? 'unknown_service',
+    version: packageJSON.version ?? 'v0.0.0',
+  };
+  return packageInfo;
+}
+
+export { loadPackageInfo, type PackageInfo };
